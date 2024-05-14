a23a9bf23408388870bef6ab66b30f7f0011a618
Aluno <kauaaapaes@gmail.com>
2024-05-14 16:31:37 -0300

    Tela cadastrar e login

---
+++ projetoSalao/projetoSalao/resources/views/cadastrar.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign up Screen</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="login-container">
+        <img src="logo23 (1).png" alt="Imagem Centralizada">
+        <form>
+            <h2> CREATE YOUR ACCOUNT!</h2>
+            <label>
+                <input type="text" name="username" placeholder="User Name">
+            </label>
+            <label>
+                <input type="password" name="password" placeholder="Password">
+            </label>
+            <label>
+                <input type="password" name="password" placeholder="Repeat Passwordd">
+            </label>
+            <div class="social-login">
+                <button class="facebook-btn">Sign up with Facebook</button>
+                <button class="google-btn">Sign up with Google</button>
+            </div>
+            <button type="submit">Continue</button>
+            <p class="register-text">You have an account? <a href="#" id="cadastrar">Sign in</a></p>
+            <p class="copyright">&copy; 2024 Key. All Rights Reserved | Design By AquaGroup</p>
+        </form>
+    </div>
+    </div>
+</body>
+</html>
---
+++ projetoSalao/projetoSalao/resources/views/login.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign in Screen</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <div class="login-container">
+        <img src="logo23 (1).png" alt="Imagem Centralizada">
+        <form>
+            <h2> ENTER IN YOUR ACCOUNT!</h2>
+            <label>
+                <input type="text" name="username" placeholder="User Name">
+            </label>
+            <label>
+                <input type="password" name="password" placeholder="Password">
+            </label>
+
+            <div class="social-login">
+                <button class="facebook-btn">Sign in with Facebook</button>
+                <button class="google-btn">Sign in with Google</button>
+            </div>
+            <button type="submit">Lgin</button>
+            <p class="register-text">Don't have an account? <a href="#" id="cadastrar">Sign up</a></p>
+            <p class="copyright">&copy; 2024 Key. All Rights Reserved | Design By AquaGroup</p>
+        </form>
+    </div>
+    </div>
+</body>
+</html>
